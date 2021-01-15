2863bcca7e8693ce08390c682c170ae23bcb2b12
Xzavier Dunn <xmandunn@gmail.com>
2021-01-15 14:14:37 -0500

    made this today

---
+++ app.py
@@ -0,0 +1,40 @@
+import csv
+import requests
+import random
+import string
+
+from flask import Flask, redirect, render_template, request, make_response, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def create():
+    if request.args.get('url'):
+        url = request.args.get('url')
+
+        try:
+            r = requests.get('https://' + url)
+        except Exception as e:
+            return jsonify('Something is wrong with your URL')
+
+        if r.status_code and r.status_code == 200:
+            letters = string.ascii_lowercase + string.digits + string.hexdigits
+            short = ''.join(random.choice(letters) for i in range(5))
+            with open('data/urls.csv', 'a') as file:
+                csvwriter = csv.writer(file)
+                csvwriter.writerow([url, short])
+            return jsonify('https://xdurl.herokuapp.com/' + short)
+
+    else:
+        return render_template('index.html')
+
+
+@app.route('/<string:id>', methods=['GET'])
+def redirectToUrl(id):
+    with open('data/urls.csv', 'r') as file:
+        for i in file.readlines():
+            i = i.split(',')
+            if i[1].strip('\n') == id:
+                return redirect('https://' + i[0])
+        return jsonify('There was an issue')
---
+++ static/index.js
@@ -0,0 +1,62 @@
+let btn = document.getElementById("btn");
+let url = document.getElementById("url");
+let copy = document.getElementById("copy");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  toggle();
+
+  url = url.value.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split("/")[0];
+  fetch(`http://127.0.0.1:5000?url=${url}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data != "Something is wrong with your URL") {
+        setInfo(data);
+      } else {
+        alert(data);
+        location.reload();
+      }
+      toggle();
+    });
+});
+
+copy.addEventListener("click", () => {
+  let urlBox = document.getElementById("urlBox");
+
+  urlBox.select();
+  urlBox.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+});
+
+let toggle = () => {
+  let spinner = document.getElementById("spinner");
+
+  if (spinner.style.display === "none") {
+    spinner.style.display = "block";
+  } else {
+    spinner.style.display = "none";
+  }
+};
+
+let setInfo = (data) => {
+  let urlBox = document.getElementById("urlBox");
+  let copy = document.getElementById("copy");
+
+  urlBox.value = data;
+  copy.innerHTML = "Copy";
+
+  toggleRes();
+};
+
+let toggleRes = () => {
+  let urlBox = document.getElementById("urlBox");
+  let copy = document.getElementById("copy");
+
+  if (urlBox.style.display == "none") {
+    urlBox.style.display = "block";
+    copy.style.display = "block";
+  } else {
+    urlBox.style.display = "none";
+    copy.style.display = "none";
+  }
+};
